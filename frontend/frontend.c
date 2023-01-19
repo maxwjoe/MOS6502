06a8f14d8209853478d95895747418c8a7bfc163
JoelLaptop <joel.r.maxwell@gmail.com>
2023-01-19 11:21:51 +1100

    Begin responsive design

--- frontend/frontend.c
+++ frontend/frontend.c
@@ -8,29 +8,35 @@
 // FE_CONTEXT : Context for the frontend to be passed to the cpu
 typedef struct fe_context
 {
-    WINDOW *stdscr;
-    WINDOW *register_window;
+    WINDOW *main;
+    WINDOW *title_window;
+    WINDOW *cpu_window;
     WINDOW *memory_window;
     WINDOW *stack_window;
+    int cycles;
+    int main_rows;
+    int main_cols;
+    int main_s_row;
+    int main_s_col;
 } *FE_CONTEXT;
 
 // cycle_callback : Callback function to execute on each CPU Cycle
 void cycle_callback(CPU c);
 
-// print_registers : Prints registers to the register window
-void print_registers(CPU c);
+// draw_title : Draws the title window
+void draw_title(CPU c, FE_CONTEXT ctx);
 
-// print_memory : Prints memory around the program counter
-void print_memory(CPU c);
+// draw_cpu : Draws the cpu window
+void draw_cpu(CPU c, FE_CONTEXT ctx);
 
-// print_stack : Prints the stack about the stack pointer
-void print_stack(CPU c);
+// get_window_dimensions : Calculates window sizes and positions
+void get_window_dimensions(FE_CONTEXT ctx, int *twr, int *twc, int *cwr, int *cwc);
 
 // === Main ===
 
 int main()
 {
-    // Setup UI
+    // Initialise UI
     initscr();
     cbreak();
     noecho();
@@ -42,17 +48,17 @@ int main()
 
     start_color();
     curs_set(0);
-    init_pair(1, COLOR_BLACK, COLOR_YELLOW);
 
     // Setup Hardware
     CPU c = CPUNew();
     Memory m = MemoryNew(0xFFFF);
     Clock clk = ClockNew();
+    ClockSetMode(clk, CM_STEP_NO_LIMIT);
 
     CPUConnectClock(c, clk);
     CPUConnectMemory(c, m);
 
-    // Setup Windows and Context
+    // Add Context, Windows and Callback to CPU
     FE_CONTEXT ctx = (FE_CONTEXT)malloc(sizeof(struct fe_context));
 
     if (!ctx)
@@ -60,257 +66,94 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-    int s_rows, s_cols;
-    getmaxyx(stdscr, s_rows, s_cols);
+    ctx->cycles = 0;
+    ctx->main = stdscr;
 
-    ctx->stdscr = stdscr;
-    ctx->register_window = newwin(s_rows / 2, s_cols / 3, 0, 2 * s_cols / 3);
-    ctx->stack_window = newwin((s_rows / 2) - 1, s_cols / 3, (s_rows / 2) + 1, 2 * s_cols / 3);
-    ctx->memory_window = newwin(s_rows, 2 * s_cols / 3, 0, 0);
+    getmaxyx(stdscr, ctx->main_rows, ctx->main_cols);
+    getbegyx(stdscr, ctx->main_s_row, ctx->main_s_col);
+
+    ctx->title_window = newwin(ctx->main_rows / 5, ctx->main_cols, ctx->main_s_row, ctx->main_s_col);
+    ctx->cpu_window = newwin(3 * ctx->main_rows / 5, ctx->main_cols / 3, ctx->main_rows / 5, 0);
 
     refresh();
 
-    CPUSetEnvContext(c, ctx);
+    CPUSetEnvContext(c, (void *)ctx);
     CPUSetCycleCallback(c, &cycle_callback);
 
-    // Program Memory
-
-    MemoryWriteByte(m, DEFAULT_PROGRAM_COUNTER, JSR_AB);
-    MemoryWriteByte(m, DEFAULT_PROGRAM_COUNTER + 1, 0x32);
-    MemoryWriteByte(m, DEFAULT_PROGRAM_COUNTER + 2, 0x45);
-    MemoryWriteByte(m, 0x4532, LDA_IM);
-    MemoryWriteByte(m, 0x4532 + 1, 0x01);
-    MemoryWriteByte(m, 0x4532 + 2, ADC_IM);
-    MemoryWriteByte(m, 0x4532 + 3, 0x02);
-
-    ClockSetTickLimit(clk, 6);
-    ClockSetMode(clk, CM_STEP_NO_LIMIT);
-
+    // Tick Clock once to see static layout
     CPUExecute(c);
-    CPUClockTick(c);
 
     // Cleanup
     getch();
     endwin();
+    free(ctx);
 
     return 0;
 }
 
 // === Function Definitions ===
 
-void print_stack(CPU c)
+void draw_title(CPU c, FE_CONTEXT ctx)
 {
-    // Unpack Windows
-    FE_CONTEXT ctx = CPUGetEnvContext(c);
+    WINDOW *terminal = ctx->main;
+    WINDOW *titlewin = ctx->title_window;
 
-    if (!ctx)
-    {
-        exit(EXIT_FAILURE);
-    }
+    box(titlewin, 0, 0);
 
-    WINDOW *stkwin = ctx->stack_window;
-    WINDOW *terminal = ctx->stdscr;
-    Memory mem_ptr = CPUGetMemory(c);
-
-    int stk_rows, stk_cols;
-    getmaxyx(stkwin, stk_rows, stk_cols);
-
-    int start_row = 2;
-    int start_col = 2;
-
-    wclear(stkwin);
-    box(stkwin, 0, 0);
-
-    // Heading
-    wattron(stkwin, A_BOLD);
-    wattron(stkwin, A_UNDERLINE);
-    mvwprintw(stkwin, start_row + 1, start_row + 2, "Stack Memory");
-    wattroff(stkwin, A_BOLD);
-    wattroff(stkwin, A_UNDERLINE);
-
-    // Print Stack
-    byte stk_ptr = CPUGetSP(c);
-    int row_limit = stk_rows - 7;
-
-    int curr_row = 0;
-
-    word addr_i = DEFAULT_STACK_BEGIN + stk_ptr;
-    while (curr_row < row_limit)
-    {
-        int is_stk_ptr = ((addr_i & 0x00FF) == stk_ptr);
-
-        if (is_stk_ptr)
-        {
-            wattron(stkwin, COLOR_PAIR(1));
-        }
-
-        // Print Stack Address
-        wattron(stkwin, A_BOLD);
-        mvwprintw(stkwin, start_row + 3 + curr_row, start_col + 2, "0x%02X : %x", stk_ptr);
-        wattroff(stkwin, A_BOLD);
-
-        mvwprintw(stkwin, start_row + 3 + curr_row, stk_cols - 6, "0x%02X", MemoryReadByte(mem_ptr, addr_i));
-
-        if (is_stk_ptr)
-        {
-            wattroff(stkwin, COLOR_PAIR(1));
-        }
-
-        curr_row++;
-        addr_i++;
-    }
-
-    wrefresh(stkwin);
+    wrefresh(titlewin);
 }
 
-void print_memory(CPU c)
+void draw_cpu(CPU c, FE_CONTEXT ctx)
 {
-    // Unpack Windows
-    FE_CONTEXT ctx = CPUGetEnvContext(c);
+    WINDOW *terminal = ctx->main;
+    WINDOW *cpuwin = ctx->cpu_window;
 
-    if (!ctx)
-    {
-        exit(EXIT_FAILURE);
-    }
+    box(cpuwin, 0, 0);
 
-    WINDOW *memwin = ctx->memory_window;
-    WINDOW *terminal = ctx->stdscr;
-    Memory mem_ptr = CPUGetMemory(c);
-
-    int mem_rows, mem_cols;
-    int start_row, start_col;
-    getbegyx(memwin, start_row, start_col);
-    getmaxyx(memwin, mem_rows, mem_cols);
-
-    // Clear, Draw Border
-    wclear(memwin);
-    box(memwin, 0, 0);
-
-    // Draw Heading
-    wattron(memwin, A_BOLD);
-    wattron(memwin, A_UNDERLINE);
-    mvwprintw(memwin, start_row + 1, start_col + 2, "Connected Memory");
-    wattroff(memwin, A_BOLD);
-    wattroff(memwin, A_UNDERLINE);
-
-    // Draw Address Space
-    int address_rows = mem_rows - 5;
-    word prog_counter = CPUGetPC(c);
-
-    word start_address = prog_counter & 0xFFF0;
-
-    word addr_i = start_address;
-    int curr_row = 0;
-
-    while (curr_row < address_rows)
-    {
-        // Print Address
-        wattron(memwin, A_BOLD);
-        mvwprintw(memwin, start_row + 3 + curr_row, start_col + 2, "0x%04X :", addr_i);
-        wattroff(memwin, A_BOLD);
-
-        // Print Data
-        for (byte i = 0x00; i <= 0x0F; i++)
-        {
-            int is_pc = ((addr_i + i) == prog_counter);
-
-            if (is_pc)
-            {
-                wattron(memwin, COLOR_PAIR(1));
-            }
-            mvwprintw(memwin, start_row + 3 + curr_row, start_col + 12 + (5 * i), "0x%02X", MemoryReadByte(mem_ptr, addr_i + i));
-            if (is_pc)
-            {
-                wattroff(memwin, COLOR_PAIR(1));
-            }
-        }
-
-        addr_i += 0x0010;
-        curr_row++;
-    }
-
-    wrefresh(memwin);
-}
-
-void print_registers(CPU c)
-{
-    // Unpack Windows
-    FE_CONTEXT ctx = CPUGetEnvContext(c);
-
-    if (!ctx)
-    {
-        exit(EXIT_FAILURE);
-    }
-
-    WINDOW *regwin = ctx->register_window;
-    WINDOW *terminal = ctx->stdscr;
-
-    int reg_rows, reg_cols;
-    int start_row = 1;
-    getmaxyx(regwin, reg_rows, reg_cols);
-
-    // Clear and Draw Border
-    wclear(regwin);
-    box(regwin, 0, 0);
-
-    // Draw Internal Registers
-    wattron(regwin, A_UNDERLINE);
-    wattron(regwin, A_BOLD);
-    mvwprintw(regwin, start_row, 2, "Internal Registers");
-    wattroff(regwin, A_BOLD);
-    wattroff(regwin, A_UNDERLINE);
-
-    // Draw Titles
-    mvwprintw(regwin, start_row + 2, 2, "Program Counter :");
-    mvwprintw(regwin, start_row + 3, 2, "Stack Pointer :");
-    mvwprintw(regwin, start_row + 4, 2, "Accumulator :");
-    mvwprintw(regwin, start_row + 5, 2, "X Register :");
-    mvwprintw(regwin, start_row + 6, 2, "Y Register :");
-
-    // Draw Values
-    wattron(regwin, A_BOLD);
-    mvwprintw(regwin, start_row + 2, reg_cols - 10, "0x%04X", CPUGetPC(c));
-    mvwprintw(regwin, start_row + 3, reg_cols - 10, "0x%02X", CPUGetSP(c));
-    mvwprintw(regwin, start_row + 4, reg_cols - 10, "0x%02X", CPUGetA(c));
-    mvwprintw(regwin, start_row + 5, reg_cols - 10, "0x%02X", CPUGetX(c));
-    mvwprintw(regwin, start_row + 6, reg_cols - 10, "0x%02X", CPUGetY(c));
-    wattroff(regwin, A_BOLD);
-
-    // Draw Processor Status
-    wattron(regwin, A_UNDERLINE);
-    wattron(regwin, A_BOLD);
-    mvwprintw(regwin, start_row + 10, 2, "Processor Flags");
-    wattroff(regwin, A_BOLD);
-    wattroff(regwin, A_UNDERLINE);
-
-    // Draw Titles
-    mvwprintw(regwin, start_row + 12, 2, "Carry :");
-    mvwprintw(regwin, start_row + 13, 2, "Zero :");
-    mvwprintw(regwin, start_row + 14, 2, "Interrupt Enable :");
-    mvwprintw(regwin, start_row + 15, 2, "Decimal Mode :");
-    mvwprintw(regwin, start_row + 16, 2, "Break :");
-    mvwprintw(regwin, start_row + 17, 2, "Overflow :");
-    mvwprintw(regwin, start_row + 18, 2, "Negative :");
-    mvwprintw(regwin, start_row + 19, 2, "Unused :");
-
-    // Draw Values
-    wattron(regwin, A_BOLD);
-    mvwprintw(regwin, start_row + 12, reg_cols - 10, "%d", CPUGetStatusFlag(c, PS_C));
-    mvwprintw(regwin, start_row + 13, reg_cols - 10, "%d", CPUGetStatusFlag(c, PS_Z));
-    mvwprintw(regwin, start_row + 14, reg_cols - 10, "%d", CPUGetStatusFlag(c, PS_I));
-    mvwprintw(regwin, start_row + 15, reg_cols - 10, "%d", CPUGetStatusFlag(c, PS_D));
-    mvwprintw(regwin, start_row + 16, reg_cols - 10, "%d", CPUGetStatusFlag(c, PS_B));
-    mvwprintw(regwin, start_row + 17, reg_cols - 10, "%d", CPUGetStatusFlag(c, PS_V));
-    mvwprintw(regwin, start_row + 18, reg_cols - 10, "%d", CPUGetStatusFlag(c, PS_N));
-    mvwprintw(regwin, start_row + 19, reg_cols - 10, "%d", CPUGetStatusFlag(c, PS_U));
-    wattron(regwin, A_BOLD);
-
-    wrefresh(regwin);
+    wrefresh(cpuwin);
 }
 
 void cycle_callback(CPU c)
 {
-    print_registers(c);
-    print_memory(c);
-    print_stack(c);
+    // Unpack Context and Check for Terminal Resize
+    FE_CONTEXT ctx = (FE_CONTEXT)CPUGetEnvContext(c);
+
+    int rs, cs;
+
+    getmaxyx(ctx->main, rs, cs);
+
+    if (rs != ctx->main_rows || cs != ctx->main_cols)
+    {
+        // Update Sizes
+        ctx->main_rows = rs;
+        ctx->main_cols = cs;
+
+        // Clear Windows
+        wclear(ctx->main);
+        wclear(ctx->title_window);
+        wclear(ctx->cpu_window);
+
+        // Resize Windows
+        int twr, twc, cwr, cwc;
+        twr = rs / 5;
+        twc = cs;
+        cwr = rs - twr;
+        cwc = cs / 3;
+
+        wresize(ctx->title_window, twr, twc);
+        wresize(ctx->cpu_window, cwr, cwc);
+
+        // Refresh Windows
+        wrefresh(ctx->main);
+        wrefresh(ctx->title_window);
+        wrefresh(ctx->cpu_window);
+    }
+
+    // Draw Data
+    draw_title(c, ctx);
+    draw_cpu(c, ctx);
+}
+
+void get_window_dimensions(FE_CONTEXT ctx, int *twr, int *twc, int *cwr, int *cwc)
+{
 }
